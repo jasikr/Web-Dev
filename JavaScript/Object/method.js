4071fef453b33ccbb327dbc0dfc1f1e69afe2f88
jasikr <102432301+jasikr@users.noreply.github.com>
2025-02-07 08:36:43 +0500

    Add files via upload

---
+++ JavaScript/Object/method.js
@@ -0,0 +1,29 @@
+function makeUser() {
+    return {
+      name: "John",
+      ref: this
+    };
+  }
+  
+  let user = makeUser();
+  
+  alert( user.ref.name ); // Error: Cannot read property 'name' of undefined
+
+
+  let ladder = {
+    step: 0,
+    up() {
+      this.step++;
+      return this;
+    },
+    down() {
+      this.step--;
+      return this;
+    },
+    showStep() {
+      alert( this.step );
+      return this;
+    }
+  };
+  
+  ladder.up().up().down().showStep().down().showStep(); // shows 1 then 0
